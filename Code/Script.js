8def3325057ae61346533c47e46da0b10e101af0
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-01-27 19:51:07 +0600

    Create Script.js

---
+++ Code/Script.js
@@ -0,0 +1,72 @@
+// console.log("Tushar");
+
+// alert("Alert")
+
+// document.write("I Love Bangladesh");
+// document.write("<h1>I Love Bangladesh</h1>");
+
+
+// document.write("Name : Tushar Ahmed </br>");
+// document.write("BSc in CSE");
+
+
+// var name = "Tushar";
+// document.write(name);
+// name = "Maruf";
+// document.write(name);
+
+
+// var num = 20;
+// num=toString(num)  
+// console.log(typeof(num));
+
+
+// var num = "20";
+// num = parseInt(num);  
+// console.log(typeof(num));
+
+
+// var n = 20.345678
+// console.log(n.toFixed())
+// console.log(n.toFixed(3))
+// console.log(n.toPrecision(4))
+
+
+// console.log(Number("12"))
+// console.log(typeof(Number("12")))
+// console.log(Number(true))
+// console.log(Number(false))
+
+
+// console.log("Md " + "Maruf " + "Ahmed " + "Tushar");
+// var firstName = "Tushar";
+// var lastName = " Ahmed";
+// var fullName = firstName + lastName;
+// document.write("My name is "+ fullName);
+
+// num1 = 10
+// num2 = 20
+// document.write("Num1 : " +num1, " Num2 : "+num2);
+
+
+// var name = "Tushar";
+// var l = name.length;
+// document.write(l);
+
+
+// var i = prompt("Enter your name : ");
+// document.write("Number of Character : " + i.length);
+
+
+// var text = "Tushar";
+// document.write(text.charAt(2));
+
+
+// var text = "Tushar Ahmed"
+// text = text.toUpperCase();
+// document.write(text);
+
+var tx1 = "My name is ";
+var tx2 = "Tushar Ahmed";
+var tx = tx1.concat(tx2);
+document.write(tx);
