0dd9de9bac79f078e876eaccea9fcd22c7fc7e76
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-01-28 20:59:41 +0600

    Update Script.js

--- Code/Script.js
+++ Code/Script.js
@@ -6,6 +6,7 @@
 // document.write("<h1>I Love Bangladesh</h1>");
 
 
+
 // document.write("Name : Tushar Ahmed </br>");
 // document.write("BSc in CSE");
 
@@ -66,7 +67,45 @@
 // text = text.toUpperCase();
 // document.write(text);
 
-var tx1 = "My name is ";
-var tx2 = "Tushar Ahmed";
-var tx = tx1.concat(tx2);
-document.write(tx);
+
+// var tx1 = "My name is ";
+// var tx2 = "Tushar Ahmed";
+// var tx = tx1.concat(tx2);
+// document.write(tx);
+
+
+// var tx = "Bangladesh";
+// var ans = tx.slice(2,5); // 2 index theke (5-2)=3 ta word dekhabe
+// document.write(ans);
+
+
+// console.log(2**3);
+// var x = 3;
+// x *= 5;
+// console.log(x);
+
+
+// var n1 = prompt("Enter first number : ");
+// var n2 = prompt("Enter second number : ");
+// n1 = parseInt(n1, 10);
+// n2 = parseInt(n2, 10);
+// var sum, sub;
+// sum = n1 + n2;
+// document.write("Summation : " + sum + "<br/>");
+// sub = n1 - n2;
+// document.write("Substraction : " + sub);
+
+
+
+// var base = parseFloat(prompt("Enter base: "));
+// var height = parseFloat(prompt("Enter height: "));
+// var area = (base * height) / 2;
+// document.write("Area of Traingle : " + area);
+
+
+
+var Fahrenheit, Celsius;
+Fahrenheit= parseFloat(prompt("Enter Fahrenheit :"));
+Celsius= (Fahrenheit-32)*(5/9);
+document.write("Celsius : ="+ Celsius);
+
