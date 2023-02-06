7a6b8b9a43974e63e19d253f73dd187b7440c58b
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-02-06 13:21:21 +0600

    Create script.js

---
+++ Code2/script.js
@@ -0,0 +1,11 @@
+                // DOM : getElemnetByID(), getClassByID()
+// document.getElementById("header");
+// document.getElementById("header").innerHTML = "Hello";
+// document.getElementById("para").innerHTML = "This is paragraph";
+// document.getElementsByClassName("xm")[0].innerHTML="HI";
+                    //  DOM: querySelector()
+// document.querySelector("#hid").innerHTML="Changed";
+// document.querySelector(".pcl").innerHTML="paragraph changed";
+// document.querySelector("p").innerHTML="paragraph tag changed";
+// document.querySelector("a");
+document.querySelector("a").innerHTML="Main";
