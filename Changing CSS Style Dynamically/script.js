321e17f819b3aaa31db1a10a520ab73c36d3588f
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-02-11 12:23:00 +0600

    Create script.js

---
+++ Changing CSS Style Dynamically/script.js
@@ -0,0 +1,14 @@
+var myVar = document.querySelector("#head_tag");
+function addStyle() {
+    // var myVar = document.querySelector("#head_tag");
+    myVar.classList.add("head_style");
+
+    // myVar.style.color = "red"; 
+    // myVar.style.fontSize = "3rem";
+    // myVar.style.fontStyle="italic";
+}
+
+function removeStyle() {
+    //var myVar = document.querySelector("#head_tag");
+    myVar.classList.remove("head_style");
+}
